031223544f1b0eb6fe930d1170f82b13c067d727
scc135 <scc135@gmail.com>
2022-02-15 19:51:26 -0500

    Fix dispatch in sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,20 @@ import * as React from "react";
 //   useNavigate,
 // } from "react-router-dom";
 import { hot } from "react-hot-loader/root";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import type { RootState, AppDispatch } from './redux/store';
 
 import Main from "./components/Main";
 import Nav from "./components/Nav";
 import Sidebar from "./components/Sidebar";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { getQuestions } from "./redux/questionSlice";
+
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
 const App = () => {
-  const dispatch = useDispatch();
 
-  const useEffect = () =>{ 
-    // fetch()
-    // .then(data => {
-    //   dispatch(getQuestions(data));
-    // })
-    dispatch(getQuestions('a'));
-  }
   return (
   <div className='App'>
     <Nav />
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,28 +1,34 @@
 const React = require("react");
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
 import type { RootState, AppDispatch } from '../redux/store';
+import { getQuestions } from "../redux/questionSlice";
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
 const Sidebar = () => {
  
-  // on load
+  const dispatch = useAppDispatch();
+
+  // pull from store
   const questions = useAppSelector(state => state.question.questions);
-  console.log(questions);
   
-  // function that takes the data from the fetch and changes state of questionList
+  // function that takes the data from the store and formats into jsx
   const createQuestionList = () => {
     const output = [];
     //if data exists
     if (questions !== null) {
       // iterate and return
       for (let i = 0;  i < questions.length; i++) {
-        output.push(<li key={i} className="SidebarQuestions"><a href='./'>{questions[i]}</a></li>)
+        output.push(<li key={i} className="SidebarQuestions" onClick={statething}>{questions[i]}</li>)
       }
       return output;
     }
     return;
+  }
+  const statething = () => {
+    dispatch(getQuestions('a'));
   }
 
   return (
