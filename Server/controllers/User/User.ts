55f96bd0728e50a01f26c906fafc6d87b1853209
scc135 <scc135@gmail.com>
2022-02-15 22:27:00 -0500

    Fix next functionality in  userController

--- Server/controllers/User/User.ts
+++ Server/controllers/User/User.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import { CrudController } from '../CrudController';
 const db = require ('../../models/model')
 
@@ -18,13 +18,15 @@ export class UserController extends CrudController {
         throw new Error("Method not implemented.");
     }
 
-    public async read(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response): Promise<AlgoList>   {
+    public async read(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response, next:express.NextFunction):Promise<void>   {
             console.log('in user controller')
             const qAlgos  = 'SELECT * FROM "public"."public.algo-questions" LIMIT 100';
             const homeQuery = await db.query(qAlgos);
             const details = homeQuery.rows;
-             console.log(details);
-            return res.locals = details ;
+            console.log(details);
+            res.locals.details = details;
+            next();
+            // return (details);
             
     }
 
