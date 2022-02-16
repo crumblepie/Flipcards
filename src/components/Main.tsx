c7f06a42c42d29a5388bc4e73410410648eba5a7
crumblepie <rainlewis@icloud.com>
2022-02-15 20:13:49 -0500

    Add light/dark mode and flip functionality

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import Card from "./Card";
+import Card from "./Flipcard";
 
 const Main = () => (
   <>
---
+++ src/redux/themeSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currTheme: "vs-dark",
+};
+
+export const themeSlice = createSlice({
+  name: "theme",
+  initialState,
+  reducers: {
+    changeTheme: (state) => {
+      state.currTheme = state.currTheme === "vs-dark" ? "vs-light" : "vs-dark";
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { changeTheme } = themeSlice.actions;
+
+export default themeSlice.reducer;
